c3aab95400fb5f4f60e96b6479acd1122ecc7421
Gustavo Falcão <gustavo.falcao01@gmail.com>
2021-03-03 23:56:40 +0000

    Add files via upload

---
+++ geoip.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+clear
+sudo apt install geoip-bin -y
+clear
+echo ""
+echo "================================================================================"
+echo "                              IP Geolocalization"
+echo "================================================================================"
+echo ""
+echo "                    ===== Digite o IP ou Domínio: ====="
+read ip
+echo ""
+echo "        ===== Quar que os dados capturados sejam salvos em nota? ====="
+echo ""
+echo "                                1 - Sim"
+echo "                                2 - Não *"
+echo ""
+echo "                           ===== Resposta: ====="
+read dados
+echo ""
+echo "                     Pressione <ENTER> para buscar dados!"
+read #pause
+echo "================================================================================"
+echo ""
+clear
+echo "================================================================================"
+echo "                             IP Geolocalization"
+echo "================================================================================"
+echo ""
+echo "                       ===== Dados encontrados: ====="
+echo ""
+if [[ $dados -eq 1 ]];
+then
+  geoiplookup $ip
+  echo ""
+  echo "============================================================================="
+  echo ""
+  echo "                    Pressione <ENTER> para ver a nota!"
+  read #pause
+  geoiplookup $ip > /home/gustavo/Documentos/Notes/GeoIP-Note.txt & gedit /home/gustavo/Documentos/Notes/GeoIP-Note.txt
+  exit
+else
+  geoiplookup $ip
+fi
+echo ""
+echo "==============================================================================="
+echo ""
+echo "                       Pressione <ENTER> para sair..."
+read #pause
+clear
